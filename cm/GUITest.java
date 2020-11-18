bc63b2786b2709b387b7079121678d3196d240d0
Bryson <brysonmunroe@gmail.com>
2020-11-18 16:08:56 -0600

    code monkey

--- cm/GUITest.java
+++ cm/GUITest.java
@@ -4,7 +4,7 @@ public class GUITest {
 
 	public static void main(String[] args) {
 		// so far, all our programs were sequential
-
+		//they reached the END - finito, " long silence - just kidding"
 	}
 
 }
